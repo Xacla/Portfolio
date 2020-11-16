0e21f01c36ae429020f8a2f31bc1c301ccefc8b9
Xacla <area184persona@gmail.com>
2020-11-16 22:52:19 +0900

    とりあえずjsonをポストできた

--- api/src/flask_api.py
+++ api/src/flask_api.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('/hoge',methods = ['POST'])
+def hoge_get_data():
+    return request.get_data()
+
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    return 'log in'
 
 if __name__ == "__main__":
     app.run(debug = True,host='0.0.0.0',port=5000)
